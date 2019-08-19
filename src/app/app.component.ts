b134e064ec8ddda115cc51858b70d47b2dc96831
Sebastian Wesołowski <sebastian@artnova.com.pl>
2019-08-19 13:29:17 +0200

    before ng if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,25 @@
 import { Component } from '@angular/core';
 
-import { GemCollection } from './gem';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+  title = 'Witaj w kursie Angular!';
+
+  selected = null;
+
   playlist = {
-    id: 1,
-    name: 'test',
-    track: 12,
-    color: '#ff00ff',
-    favorite: true
+    name: 'The best of EduWeb!',
+    tracks: 23,
+    color: '#FF0000',
+    favourite: true
   };
 
-  playlistArr = [
-    {
-      id: 1,
-      name: 'test',
-      track: 12,
-      color: '#ff00ff',
-      favorite: true
-    },
-    {
-      id: 2,
-      name: 'test',
-      track: 12,
-      color: '#ff00ff',
-      favorite: true
-    }
-  ];
-
-  size = 1.2;
-
-  styleBorder = '2px solid' + this.playlist.color;
-
-  gemList = [
-    new GemCollection(1, 'normal', 'basic cristal', 20),
-    new GemCollection(2, 'normal', 'cristal', 21),
-    new GemCollection(3, 'extra', 'extra cristal', 28),
-    new GemCollection(4, 'epic', 'epic cristal', 50)
-  ];
-  selected = false;
-
-  getBorderStyle(playlist) {
-    console.log(typeof playlist);
-    return {
-      borderColor: playlist.color,
-      borderRight: 'solid 4px ' + playlist.color
-    };
+  save(event) {
+    console.log('Zapisano', event);
   }
-  onSave(event) {
-    console.log('save', event);
-  }
+
+  constructor() {}
 }
