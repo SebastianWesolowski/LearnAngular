e3f806df73a559905734def50880be2abe6d3b36
Sebastian Wesołowski <sebastian@artnova.com.pl>
2019-08-19 12:38:31 +0200

    add auto foramt with hoosky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { GemCollection } from './gem';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'test';
@@ -14,7 +14,7 @@ export class AppComponent {
     name: 'test',
     track: 12,
     color: '#ff00ff',
-    favorite: true,
+    favorite: true
   };
 
   playlistArr = [
@@ -23,35 +23,35 @@ export class AppComponent {
       name: 'test',
       track: 12,
       color: '#ff00ff',
-      favorite: true,
+      favorite: true
     },
     {
       id: 2,
       name: 'test',
       track: 12,
       color: '#ff00ff',
-      favorite: true,
-    },
+      favorite: true
+    }
   ];
 
   size = 1.2;
 
   styleBorder = '2px solid' + this.playlist.color;
 
-  getBorderStyle(playlist) {
-    console.log(typeof playlist);
-    return {
-      borderColor: playlist.color,
-      borderRight: 'solid 4px ' + playlist.color,
-    };
-  }
   gemList = [
     new GemCollection(1, 'normal', 'basic cristal', 20),
     new GemCollection(2, 'normal', 'cristal', 21),
     new GemCollection(3, 'extra', 'extra cristal', 28),
-    new GemCollection(4, 'epic', 'epic cristal', 50),
+    new GemCollection(4, 'epic', 'epic cristal', 50)
   ];
   selected = false;
+  getBorderStyle(playlist) {
+    console.log(typeof playlist);
+    return {
+      borderColor: playlist.color,
+      borderRight: 'solid 4px ' + playlist.color
+    };
+  }
   onSave(event) {
     console.log('save', event);
   }
